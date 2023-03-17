be481d84eea3c4d1033a202f237bb5ae149edd01
Divyesh-Sar2003 <119989934+Divyesh-Sar2003@users.noreply.github.com>
2023-03-17 15:50:25 +0530

    ok

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(document).ready(function()
 {
     $(".select").hide();
     $(".start_btn").click(function(){
-        $(".start").hide();
-        $(".select").show();
+        $(".start").hide(1000);
+        $(".select").show(1000);
     });
     $(".html").click(function(){
         
